7b67d858339116eeb1bbaf0ab1b9522c8418c497
OpcodeMatrix <112854111+OpcodeMatrix@users.noreply.github.com>
2022-09-19 11:02:31 +0300

    Add files via upload

---
+++ src/parser.py
@@ -0,0 +1,23 @@
+class Token:
+    def __init__(self, tType, value):
+        self.tokenType = tType
+        self.tokenValue = value
+
+tokenList = []
+
+rawinput = input("Sisesta avaldis: ").strip(' ')
+last = 0
+for i in range(len(rawinput)):
+    if (rawinput[i] == '('):
+        tokenList.append(Token("operand", rawinput[i]))
+        last = i + 1
+    elif rawinput[i] in ['+', '-', '*', '/', ')']:
+        if (i - last != 0):
+            tokenList.append(Token("number", float(rawinput[last:i])))
+        tokenList.append(Token("operand", rawinput[i]))
+        last = i + 1
+if (last != len(rawinput)):
+    tokenList.append(Token("number", float(rawinput[last:len(rawinput)])))
+
+for i in range(len(tokenList)):
+    print(tokenList[i].tokenType, tokenList[i].tokenValue)
