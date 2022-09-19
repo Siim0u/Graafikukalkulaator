bef7a2864a0c7c70ba5fdac5617657374aad5bee
OpcodeMatrix <112854111+OpcodeMatrix@users.noreply.github.com>
2022-09-19 22:33:28 +0300

    Update parser.py

--- src/parser.py
+++ src/parser.py
@@ -4,18 +4,16 @@ class Token:
         self.tokenValue = value
 
 tokenList = []
-
 rawinput = input("Sisesta avaldis: ").strip(' ')
+
 last = 0
 for i in range(len(rawinput)):
-    if (rawinput[i] == '('):
-        tokenList.append(Token("operand", rawinput[i]))
-        last = i + 1
-    elif rawinput[i] in ['+', '-', '*', '/', ')']:
-        if (i - last != 0):
+    if (rawinput[i] in ['+', '-', '*', '/', '(', ')']):
+        if i - last != 0:
             tokenList.append(Token("number", float(rawinput[last:i])))
         tokenList.append(Token("operand", rawinput[i]))
         last = i + 1
+
 if (last != len(rawinput)):
     tokenList.append(Token("number", float(rawinput[last:len(rawinput)])))
 
