f88d29980a476ba06d89755c98bde4dcf00c8cc9
OpcodeMatrix <112854111+OpcodeMatrix@users.noreply.github.com>
2022-11-06 21:18:51 +0200

    Add files via upload

---
+++ src/Graafikukalkulaator.py
@@ -0,0 +1,275 @@
+import math, tkinter
+
+class Token:
+    def __init__(self, type, value, function = 0):
+        self.type = type
+        self.value = value
+        self.function = function
+
+class TextBoxDesc:
+    def __init__(self, tempText):
+      self.tempText = tempText
+      self.tempTextActive = True
+
+controls = tkinter.Tk()
+controls.title("Sisend")
+controls.geometry("400x200")
+
+widgets = []
+
+x_start = tkinter.ttk.Entry(controls)
+widgets.append([x_start, TextBoxDesc("X-telje algus")])
+x_start.place(x=50, y=50, width=75)
+x_start.insert(0, "X-telje algus")
+x_start.config(foreground = 'grey')
+x_end = tkinter.ttk.Entry(controls)
+widgets.append([x_end, TextBoxDesc("X-telje lõpp")])
+x_end.insert(0, "X-telje lõpp")
+x_end.config(foreground = 'grey')
+x_end.place(x=150, y=50, width=75)
+
+y_start = tkinter.ttk.Entry(controls)
+widgets.append([y_start, TextBoxDesc("Y-telje algus")])
+y_start.insert(0, "Y-telje algus")
+y_start.config(foreground = 'grey')
+y_start.place(x=50, y=100, width=75)
+y_end = tkinter.ttk.Entry(controls)
+widgets.append([y_end, TextBoxDesc("Y-telje lõpp")])
+y_end.insert(0, "Y-telje lõpp")
+y_end.config(foreground = 'grey')
+y_end.place(x=150, y=100, width=75)
+
+entry = tkinter.ttk.Entry(controls)
+widgets.append([entry, TextBoxDesc("Funktsiooni avaldis")])
+entry.insert(0, "Funktsiooni avaldis")
+entry.config(foreground = 'grey')
+entry.place(x=50, y=150, width=150)
+
+canvas_width = 1000
+canvas_height = 1000
+
+master = tkinter.Tk()
+master.geometry(f"{canvas_width + 40}x{canvas_height + 40}")
+graphWindow = tkinter.Canvas(master, width=canvas_width, height=canvas_height)
+master.title("Graafik")
+graphWindow.place(x=40, y=10)
+
+xLabels = []
+yLabels = []
+
+for x in range(int(canvas_width / 100) + 1):
+    xLabels.append(tkinter.Label(master, text='0'))
+    xLabels[x].place(x=x * 100 + 20, y=canvas_height+20)
+
+for y in range(int(canvas_height / 100) + 1):
+    yLabels.append(tkinter.Label(master, text='0'))
+    yLabels[y].place(x=0, y=y * 100)
+
+def on_entry_click(event):
+    widget = event.widget
+    for i in widgets:
+        if i[0] == widget and i[1].tempTextActive:
+            widget.delete(0, "end")
+            widget.insert(0, '') 
+            widget.config(foreground = 'black')
+            i[1].tempTextActive = False
+            break
+        
+def on_focusout(event):
+    widget = event.widget
+    for i in widgets:
+        if i[0] == widget and widget.get() == '':
+            widget.insert(0, i[1].tempText)
+            widget.config(foreground = 'grey')
+            i[1].tempTextActive = True
+            break
+   
+x_start.bind("<FocusIn>", on_entry_click)
+x_start.bind("<FocusOut>", on_focusout)
+x_end.bind("<FocusIn>", on_entry_click)
+x_end.bind("<FocusOut>", on_focusout)
+y_start.bind("<FocusIn>", on_entry_click)
+y_start.bind("<FocusOut>", on_focusout)
+y_end.bind("<FocusIn>", on_entry_click)
+y_end.bind("<FocusOut>", on_focusout)
+entry.bind("<FocusIn>", on_entry_click)
+entry.bind("<FocusOut>", on_focusout)
+
+def CreateGraph():
+    tokenList = []
+    readerState = 0
+    last = 0
+    rawinput = entry.get()
+    for i in range(len(rawinput)):
+        if rawinput[i] >= '0' and rawinput[i] <= '9' and (readerState & 3) == 0:
+            last = i
+            readerState |= 1
+        elif rawinput[i] >= 'a' and rawinput[i] <= 'z' and (readerState & 3) == 0:
+            last = i
+            readerState |= 2
+        elif rawinput[i] in ['+', '-', '*', '/', '(', ')']:
+            if (readerState & 4) != 0 and rawinput[i] == '-':
+                last = i
+                readerState |= 1
+                readerState &= ~4
+            else:
+                if (readerState & 1) != 0:
+                    tokenList.append(Token("number", float(rawinput[last:i])))
+                    readerState &= ~1
+                if (readerState & 2) == 0:
+                    tokenList.append(Token("operand", rawinput[i]))
+                else:
+                    functionName = rawinput[last:i]
+                    if functionName == "x":
+                        tokenList.append(Token("x", 0))
+                        tokenList.append(Token("operand", rawinput[i]))
+                    else:
+                        tokenList.append(Token("operand", rawinput[i], rawinput[last:i]))
+                    readerState &= ~2
+            
+            if rawinput[i] == '(':
+                readerState |= 4
+        elif (readerState & 4) != 0:
+            readerState &= ~4
+
+    if (readerState & 1) != 0:
+        tokenList.append(Token("number", float(rawinput[last:len(rawinput)])))
+    elif (readerState & 2) != 0:
+        tokenList.append(Token("x", 0))
+    
+    xstart = float(x_start.get())
+    ystart = float(y_start.get())
+    xend = float(x_end.get())
+    yend = float(y_end.get())
+    xChangePerPixel = (abs(xstart) + abs(xend)) / canvas_width
+    yChangePerPixel = (abs(ystart) + abs(yend)) / canvas_height
+    
+    yNegativeRatio = abs(yend) / (abs(ystart) + abs(yend))
+    yoffset = canvas_height * yNegativeRatio
+
+    graphWindow.delete("all")
+    
+    for x in range(int(canvas_width / 100) + 1):
+        xLabels[x].config(text=str(round(xstart + xChangePerPixel * x * 100, 3)))
+        graphWindow.create_line(x * 100, 0, x * 100, canvas_height, fill="lightgray")
+    
+    for y in range(int(canvas_height / 100) + 1):
+        yLabels[y].config(text=str(round(yend + yChangePerPixel * -y * 100, 3)))
+        graphWindow.create_line(0, y * 100, canvas_width, y * 100, fill="lightgray")
+    
+    for x in range(canvas_width):
+        error = False
+        y1, error = Compute(tokenList.copy(), xstart + xChangePerPixel * x)
+        y2, error = Compute(tokenList.copy(), xstart + xChangePerPixel * (x + 1))
+        if not error:
+            graphWindow.create_line(x, -(y1 / yChangePerPixel) + yoffset, x + 1, -(y2 / yChangePerPixel) + yoffset)
+         
+start = tkinter.ttk.Button(controls, text="Loo graafik", command=CreateGraph)
+start.place(x=300, y=100, width=75)
+
+def GetOperation(tokenList, offset, xvalue, globalError):
+    currentOperation = [0, 0, 0]
+    for j in range(3):
+        i = j + offset
+        if tokenList[i].value == '(':
+            newParenthesis = 0
+            closingIndex = i + 1
+            while not (tokenList[closingIndex].value == ')' and newParenthesis == 0):
+                if tokenList[closingIndex].value == '(':
+                    newParenthesis += 1
+                elif tokenList[closingIndex].value == ')':
+                    newParenthesis -= 1
+                closingIndex += 1
+            error = False
+            tokenVal, error = Compute(tokenList[i + 1:closingIndex], xvalue, tokenList[i].function, globalError)
+            if error:
+                globalError = True
+            parenthesisValue = Token("number", tokenVal)
+            currentOperation[j] = parenthesisValue.value
+            tokenList[i] = parenthesisValue
+            del tokenList[i + 1:closingIndex + 1]
+            if (len(tokenList) == 1):
+                break
+        else:
+            if tokenList[i].type == "x":
+                currentOperation[j] = xvalue
+            else:
+                currentOperation[j] = tokenList[i].value
+    return currentOperation, globalError
+
+def ComputeOperation(operation, globalError):
+    value = 0
+    if operation[1] == '*':
+        value = operation[0] * operation[2]
+    elif operation[1] == '/':
+        if operation[2] != 0:
+            value = operation[0] / operation[2]
+        else:
+            #globalError = True
+            return Token("number", 0)
+    elif operation[1] == '+':
+        value = operation[0] + operation[2]
+    elif operation[1] == '-':
+        value = operation[0] - operation[2]
+    return Token("number", value)
+
+def Compute(tokenList, xvalue, rawfunction = 0, globalError = False):
+    while len(tokenList) > 1:
+        currentOperation, globalError = GetOperation(tokenList, 0, xvalue, globalError)
+        if currentOperation[1] in ['*', '/']:   # Kui tehe on * või /, siis saab selle väärtuse kohe välja arvutada
+            tokenList[0] = ComputeOperation(currentOperation, globalError)
+            del tokenList[1:3]
+        elif currentOperation[1] in ['+', '-']:
+            if len(tokenList) <= 3: # Kui tehe on + või - ja see on viimane allesjäänud tehe, võib selle kohe välja arvutada
+                tokenList[0] = ComputeOperation(currentOperation, globalError)
+                del tokenList[1:3]
+            elif GetOperation(tokenList, 2, xvalue, globalError)[1] in ['+', '-']:   # Kui + või - tehe ei ole viimane, kuid ka sellest järgmine tehe on + või -, võib kohe arvutada
+                tokenList[0] = ComputeOperation(currentOperation, globalError)
+                del tokenList[1:3]
+            else:   # Kui + või - tehe ei ole viimane, ja sellest järgmine tehe on * või /, arvutatakse kohe välja järgmise tehte väärtus ja eelmine tehe arvutatakse välja järgmisel tsüklil, kui see on siis võimalik
+                nextOperation, globalError = GetOperation(tokenList, 2, xvalue, globalError)
+                tokenList[2] = ComputeOperation(nextOperation)
+                del tokenList[3:5]
+    
+    if tokenList[0].type == "x":
+        tokenList[0] = Token("number", xvalue)
+
+    if rawfunction == 0:
+        return tokenList[0].value, globalError
+    else:
+        i = 0
+        while i < len(rawfunction):
+            if rawfunction[i] < 'a':
+                break
+            i += 1
+        functionName = rawfunction[0:i]
+        functionValue = 0
+        if i != len(rawfunction):
+            functionValue = float(rawfunction[i:len(rawfunction)])
+
+        error = False
+        newvalue = 0
+        if functionName == "sin":
+            newvalue = math.sin(tokenList[0].value)
+        elif functionName == "cos":
+            newvalue = math.cos(tokenList[0].value)
+        elif functionName == "tan":
+            newvalue = math.tan(tokenList[0].value)
+        elif functionName == "pow":
+            newvalue = tokenList[0].value ** functionValue
+        elif functionName == "rt":
+            if tokenList[0].value > 0:
+                newvalue = tokenList[0].value ** (1/functionValue)
+            elif tokenList[0].value < 0:
+                globalError = True
+        elif functionName == "log":
+            if tokenList[0].value > 0:
+                newvalue = math.log(tokenList[0].value) / math.log(functionValue)
+            else:
+                globalError = True
+        elif functionName == "ln":
+            if tokenList[0].value > 0:
+                newvalue = math.log(tokenList[0].value) / math.log(math.e)
+            else:
+                globalError = True
+        return newvalue, globalError
