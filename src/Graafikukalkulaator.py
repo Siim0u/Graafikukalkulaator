a8ec08a1838147c90008d92573bcf4f2706b3b2f
OpcodeMatrix <112854111+OpcodeMatrix@users.noreply.github.com>
2022-11-13 16:18:56 +0200

    Update Graafikukalkulaator.py

--- src/Graafikukalkulaator.py
+++ src/Graafikukalkulaator.py
@@ -11,57 +11,66 @@ class EntryDesc:
       self.tempText = tempText
       self.tempTextActive = True
 
-widgets = []
-
 canvas_width = 800
-canvas_height = 900
-graph_height = 800
-
+canvas_height = 800
 CANVAS_OFFSET = 2
 
+widgets = []
+
 master = tkinter.Tk()
-master.geometry(f"{canvas_width + 50 + CANVAS_OFFSET}x{canvas_height + 40 + CANVAS_OFFSET}")
+master.geometry(f"{canvas_width + 50 + CANVAS_OFFSET}x{canvas_height + 140 + CANVAS_OFFSET}")
 graphWindow = tkinter.Canvas(master, width=canvas_width + CANVAS_OFFSET, height=canvas_height + CANVAS_OFFSET)
 master.title("Graafik")
 graphWindow.place(x=40, y=0)
 
+UI_START_X = 40
+UI_START_Y = 880
+
 x_start = tkinter.ttk.Entry(master)
 widgets.append([x_start, EntryDesc("X-telje algus")])
-x_start.place(x=50, y=875, width=75)
+x_start.place(x=UI_START_X + 20, y=UI_START_Y, width=75)
 x_start.insert(0, "X-telje algus")
 x_start.config(foreground = 'grey')
 x_end = tkinter.ttk.Entry(master)
 widgets.append([x_end, EntryDesc("X-telje lõpp")])
 x_end.insert(0, "X-telje lõpp")
 x_end.config(foreground = 'grey')
-x_end.place(x=150, y=875, width=75)
+x_end.place(x=UI_START_X + 100, y=UI_START_Y, width=75)
 
 y_start = tkinter.ttk.Entry(master)
 widgets.append([y_start, EntryDesc("Y-telje algus")])
 y_start.insert(0, "Y-telje algus")
 y_start.config(foreground = 'grey')
-y_start.place(x=250, y=875, width=75)
+y_start.place(x=UI_START_X + 20, y=UI_START_Y + 30, width=75)
 y_end = tkinter.ttk.Entry(master)
 widgets.append([y_end, EntryDesc("Y-telje lõpp")])
 y_end.insert(0, "Y-telje lõpp")
 y_end.config(foreground = 'grey')
-y_end.place(x=350, y=875, width=75)
+y_end.place(x=UI_START_X + 100, y=UI_START_Y + 30, width=75)
+
+tkinter.Label(master, text="X:").place(x=UI_START_X, y=UI_START_Y)
+tkinter.Label(master, text="Y:").place(x=UI_START_X, y=UI_START_Y + 30)
+tkinter.Label(master, text="f(x) =").place(x=UI_START_X + 180, y=UI_START_Y)
 
 entry = tkinter.ttk.Entry(master)
 widgets.append([entry, EntryDesc("Funktsiooni avaldis")])
 entry.insert(0, "Funktsiooni avaldis")
 entry.config(foreground = 'grey')
-entry.place(x=450, y=875, width=150)
+entry.place(x=UI_START_X + 220, y=UI_START_Y, width=150)
 
+functionValueLabel = tkinter.Label(master, text="f(x) =")
+functionValueLabel.place(x=40, y=canvas_height + 50)
+functionValueEntry = tkinter.ttk.Entry(master)
+functionValueEntry.place(x=50 + tkinter.font.nametofont("TkDefaultFont").measure("f(x) ="), y=canvas_height + 50, width=100)
 
 xLabels = []
 yLabels = []
 
 for x in range(int(canvas_width / 100) + 1):
     xLabels.append(tkinter.Label(master, text='0'))
-    xLabels[x].place(x=x * 100 + 20, y=graph_height+20)
+    xLabels[x].place(x=x * 100 + 20, y=canvas_height+20)
 
-for y in range(int(graph_height / 100) + 1):
+for y in range(int(canvas_height / 100) + 1):
     yLabels.append(tkinter.Label(master, text='0'))
     yLabels[y].place(x=0, y=y * 100)
 
@@ -73,7 +82,7 @@ def OnEntryClick(event):
             widget.insert(0, '') 
             widget.config(foreground = 'black')
             i[1].tempTextActive = False
-            break
+            return
         
 def OnEntryFocusOut(event):
     widget = event.widget
@@ -82,8 +91,16 @@ def OnEntryFocusOut(event):
             widget.insert(0, i[1].tempText)
             widget.config(foreground = 'grey')
             i[1].tempTextActive = True
-            break
-   
+            return
+
+def OnMouseMove(event, xstart, xChangePerPixel, tokenList):
+    x = xstart + xChangePerPixel * event.x
+    functionLabelText = f"f({str(round(x, 3))}) ="
+    functionValueLabel.config(text=functionLabelText)
+    functionValueEntry.delete(0, "end")
+    functionValueEntry.insert(0, str(Compute(tokenList.copy(), x)[0]))
+    functionValueEntry.place(x=50 + tkinter.font.nametofont("TkDefaultFont").measure(functionLabelText))
+
 x_start.bind("<FocusIn>", OnEntryClick)
 x_start.bind("<FocusOut>", OnEntryFocusOut)
 x_end.bind("<FocusIn>", OnEntryClick)
@@ -127,8 +144,8 @@ def CreateGraph():
                         tokenList.append(Token("operand", rawinput[i], rawinput[last:i]))
                     readerState &= ~2
             
-            if rawinput[i] == '(':
-                readerState |= 4
+        if rawinput[i] == '(':
+            readerState |= 4
         elif (readerState & 4) != 0:
             readerState &= ~4
 
@@ -141,31 +158,33 @@ def CreateGraph():
     ystart = float(y_start.get())
     xend = float(x_end.get())
     yend = float(y_end.get())
-    xChangePerPixel = (abs(xstart) + abs(xend)) / canvas_width
-    yChangePerPixel = (abs(ystart) + abs(yend)) / graph_height
+    xChangePerPixel = (abs(xend - xstart)) / canvas_width
+    yChangePerPixel = (abs(yend - ystart)) / canvas_height
     
-    yRatio = yend / (abs(ystart) + abs(yend))
-    yoffset = graph_height * yRatio
+    yRatio = yend / abs(yend - ystart)
+    yoffset = canvas_height * yRatio
 
     graphWindow.delete("all")
+    graphWindow.bind("<Motion>", lambda event:OnMouseMove(event, xstart, xChangePerPixel, tokenList.copy()))
     
     for x in range(int(canvas_width / 100) + 1):
         xLabels[x].config(text=str(round(xstart + xChangePerPixel * x * 100, 3)))
-        graphWindow.create_line(x * 100 + CANVAS_OFFSET, 0, x * 100 + CANVAS_OFFSET, graph_height + CANVAS_OFFSET, fill="lightgray")
+        graphWindow.create_line(x * 100 + CANVAS_OFFSET, 0, x * 100 + CANVAS_OFFSET, canvas_height + CANVAS_OFFSET, fill="lightgray")
     
-    for y in range(int(graph_height / 100) + 1):
+    for y in range(int(canvas_height / 100) + 1):
         yLabels[y].config(text=str(round(yend + yChangePerPixel * -y * 100, 3)))
         graphWindow.create_line(0, y * 100 + CANVAS_OFFSET, canvas_width + CANVAS_OFFSET, y * 100 + CANVAS_OFFSET, fill="lightgray")
     
+    y2, error2 = Compute(tokenList.copy(), xstart)
     for x in range(canvas_width):
-        error = False
-        y1, error = Compute(tokenList.copy(), xstart + xChangePerPixel * x)
-        y2, error = Compute(tokenList.copy(), xstart + xChangePerPixel * (x + 1))
-        if not error:
+        y1 = y2
+        error1 = error2
+        y2, error2 = Compute(tokenList.copy(), xstart + xChangePerPixel * (x + 1))
+        if not (error1 or error2):
             graphWindow.create_line(x + CANVAS_OFFSET, -(y1 / yChangePerPixel) + yoffset + CANVAS_OFFSET, x + 1 + CANVAS_OFFSET, -(y2 / yChangePerPixel) + yoffset + CANVAS_OFFSET)
          
 start = tkinter.ttk.Button(master, text="Loo graafik", command=CreateGraph)
-start.place(x=650, y=875, width=75, height=50)
+start.place(x=UI_START_X + 400, y=UI_START_Y, width=75, height=50)
 
 def GetOperation(tokenList, offset, xvalue, globalError):
     currentOperation = [0, 0, 0]
@@ -231,7 +250,7 @@ def Compute(tokenList, xvalue, rawfunction = 0, globalError = False):
             # Kui + või - tehe ei ole viimane, ja sellest järgmine tehe on * või /, arvutatakse kohe välja järgmise tehte väärtus ja eelmine tehe arvutatakse välja järgmisel tsüklil, kui see on siis võimalik
             else:
                 nextOperation, globalError = GetOperation(tokenList, 2, xvalue, globalError)
-                tokenList[2] = ComputeOperation(nextOperation)
+                tokenList[2] = ComputeOperation(nextOperation, globalError)
                 del tokenList[3:5]
     
     if tokenList[0].type == "x":
@@ -267,12 +286,12 @@ def Compute(tokenList, xvalue, rawfunction = 0, globalError = False):
                 globalError = True
         elif functionName == "log":
             if tokenList[0].value > 0:
-                newvalue = math.log(tokenList[0].value) / math.log(functionValue)
+                newvalue = math.log(tokenList[0].value, 10 if functionValue == 0 else functionValue)
             else:
                 globalError = True
         elif functionName == "ln":
             if tokenList[0].value > 0:
-                newvalue = math.log(tokenList[0].value) / math.log(math.e)
+                newvalue = math.log(tokenList[0].value, math.e)
             else:
                 globalError = True
         return newvalue, globalError
